1dbd4cf0e9f65965ddc2a1361b8e95bd0a845e86
matumasadev02 <77044083+matumasadev02@users.noreply.github.com>
2021-10-10 11:38:12 +0900

    update main.js

--- main.js
+++ main.js
@@ -1,37 +1,41 @@
 fetch(
-  "https://script.google.com/macros/s/AKfycbyemSqGzoPqjVaQF2OzTXGfCp9uwsIsqfol_zlytD7Qpu3otU6jpouAJh84edMF_JWv-g/exec"
+  "https://script.google.com/macros/s/AKfycbzL6ucRKn-e-r1CSDF_vNRVZ_xNyXHUtshutYqTJvS1XBinp_CbuLv6mtaJZCLkPvbEbg/exec"
 )
   .then((res) => {
     return res.json();
   })
   .then((json) => {
-    return json.words;
-  })
-  .then((words) => {
     var wordsCount = 0;
-    words.forEach((wordSet) => {
-      wordsCount += 1;
-      var newUlEl = document.createElement("ul");
+    json.words.forEach((wordSet) => {
+      wordsCount += 1; //何番目か
+      var newUlEl = document.createElement("ul"); //ulElを定義
       var newUl = document
         .getElementById("words-container")
-        .appendChild(newUlEl);
+        .appendChild(newUlEl); //newUlを定義、ulをHTMLに作成
+      var wordCount = 0;
       wordSet.forEach((word) => {
-        var newLiEl = document.createElement("li");
-        var newLi = newUl.appendChild(newLiEl);
+        //wordSetからwordを取り出す
+        var newLiEl = document.createElement("li"); //liElを定義
+        var newLi = newUl.appendChild(newLiEl); //newLiを定義、liをHTMLに作成
         newLi.className = "cover";
-        newLi.innerHTML = `<span>${word}</span>`;
+        newLi.innerHTML = `<span>${json.groups[0][wordCount]}</span><span>${word}</span>`; //liの子要素spanにwordを格納
         newLi.addEventListener("click", function () {
+          //addEventListenerでクリックを検出し、coverクラスをtoggle
           this.classList.toggle("cover");
         });
+        wordCount += 1;
       });
-      var checkBtnEl = document.createElement("button");
-      checkBtnEl.innerHTML = "覚えた！";
-      checkBtnEl.value = wordsCount;
-      var newBtn = newUl.appendChild(checkBtnEl);
+      //checkBtnの実装
+      var checkBtnEl = document.createElement("button"); //checkBtnElを定義
+      checkBtnEl.innerHTML = "覚えた！"; //innerHTMLで内容を入れる
+      checkBtnEl.value = wordsCount; //hiddenをつけるul要素が何番目かをvalueに入れる
+      var newBtn = newUl.appendChild(checkBtnEl); //newBtnをHTMLに作成
       newBtn.addEventListener("click", () => {
+        //addEventListenerでクリックを検出し、hiddenクラスをadd
         newUl.classList.add("hidden");
       });
     });
+    // }
   })
   .catch((e) => {
     document.getElementById("words-container").innerHTML =
