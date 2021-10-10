ca56af049be483dbbd6c9b6051ae5297412ca296
matumasadev02 <77044083+matumasadev02@users.noreply.github.com>
2021-10-10 22:12:19 +0900

    update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+function pushData(array, value) {
+  if (array.indexOf(value) === -1) {
+    array.push(value);
+  }
+  return true;
+}
+
 function getSheetList() {
   fetch(
     "https://script.google.com/macros/s/AKfycbxN6EBW6CNO7P6aIzmHulc6qGLKeNLVRLYHsww-abC_6W_f946m_1YR95LWeGsDpFXdxQ/exec?action=getSheetNames"
@@ -7,12 +14,10 @@ function getSheetList() {
     })
     .then((json) => {
       let sheetNames = json.sheetNames;
-      console.log(sheetNames);
-      let namesCount = 0;
-      sheetNames.forEach((sheetName) => {
+      for (let sheetIndex = 0; sheetIndex < sheetNames.length; ++sheetIndex) {
         let selectLink = document.createElement("button");
-        selectLink.value = namesCount;
-        selectLink.innerText = sheetName;
+        selectLink.value = sheetIndex;
+        selectLink.innerText = sheetNames[sheetIndex];
         let newSelectLink = document
           .getElementById("select")
           .appendChild(selectLink);
@@ -20,12 +25,13 @@ function getSheetList() {
           clearWord();
           getWord(this.value);
         });
-        namesCount += 1;
-      });
+      }
+      hiddenUpdate();
     });
 }
 function clearWord() {
   document.getElementById("words-container").innerHTML = "";
+  hiddenWords = getHidden();
 }
 function getWord(sheetId) {
   fetch(
@@ -35,39 +41,59 @@ function getWord(sheetId) {
       return res.json();
     })
     .then((json) => {
+      let words = json.words;
       document.getElementById("name").innerText = json.name;
-      json.words.forEach((wordSet) => {
+      for (let wordIndex = 0; wordIndex < words.length; ++wordIndex) {
         var newUlEl = document.createElement("ul"); //ulElを定義
         var newUl = document
           .getElementById("words-container")
           .appendChild(newUlEl); //newUlを定義、ulをHTMLに作成
-        var wordCount = 0;
-        wordSet.forEach((word) => {
+        for (
+          let wordCount = 0;
+          wordCount < words[wordIndex].length;
+          ++wordCount
+        ) {
           //wordSetからwordを取り出す
           var newLiEl = document.createElement("li"); //liElを定義
           var newLi = newUl.appendChild(newLiEl); //newLiを定義、liをHTMLに作成
           newLi.className = "cover";
-          newLi.innerHTML = `<span>${json.groups[0][wordCount]}</span><span>${word}</span>`; //liの子要素spanにwordを格納
+          newLi.innerHTML = `<span>${json.groups[0][wordCount]}</span><span>${words[wordIndex][wordCount]}</span>`; //liの子要素spanにwordを格納
           newLi.addEventListener("click", function () {
             //addEventListenerでクリックを検出し、coverクラスをtoggle
             this.classList.toggle("cover");
           });
-          wordCount += 1;
-        });
+        }
         //checkBtnの実装
         var checkBtnEl = document.createElement("button"); //checkBtnElを定義
         checkBtnEl.innerHTML = "覚えた！"; //innerHTMLで内容を入れる
         var newBtn = newUl.appendChild(checkBtnEl); //newBtnをHTMLに作成
         newBtn.addEventListener("click", () => {
-          //addEventListenerでクリックを検出し、hiddenクラスをadd
-          newUl.classList.add("hidden");
+          pushData(hiddenWords, wordIndex);
+          console.log(hiddenWords);
+          document
+            .getElementById("words-container")
+            .childNodes[wordIndex].classList.add("hidden");
         });
-      });
-      // }
+      }
     })
     .catch((e) => {
       document.getElementById("words-container").innerHTML =
         '<p>An error occurred.</p><p>Your browser may not support fetch. Please use <a href="https://google.com/chrome">Google Chome Browser</a>.</p>';
     });
 }
-window.onload = getSheetList()
+function getHidden() {
+  return [];
+}
+function hiddenUpdate() {
+  hiddenWords.forEach((hiddenword) => {
+    document
+      .getElementById("words-container")
+      .childNodes[hiddenword].classList.add("hidden");
+  });
+  document.cookie = `hiddenWords = ${hiddenWords}`;
+}
+let hiddenWords = getHidden();
+function main() {
+  getSheetList();
+}
+window.onload = main();
